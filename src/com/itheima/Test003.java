0caf101d266aa06d34a3bf2673d21ae91a2b290d
wangzhijie <710771031@qq.com>
2019-08-22 00:34:39 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/itheima/Test002.java

--- src/com/itheima/Test003.java
+++ src/com/itheima/Test003.java
@@ -1,6 +1,6 @@
 package com.itheima;
 
-public class Test002 {
+public class Test003 {
     public static void main(String[] args) {
         System.out.println("test002-blog");
         System.out.println("test002-blog222222222222");
