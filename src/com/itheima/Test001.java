79c112b5d1be8a1bb38a40a3d4b070a9429cf5b8
wangzhijie <710771031@qq.com>
2019-08-21 21:44:57 +0800

    clone 第一次修改

--- src/com/itheima/Test001.java
+++ src/com/itheima/Test001.java
@@ -4,5 +4,6 @@ public class Test001 {
     public static void main(String[] args) {
         System.out.println("第一次");
         System.out.println("第二次");
+        System.out.println("clone第一次修改");
     }
 }
